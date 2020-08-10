aa7b1e3331104cf1e3682c1c1cb228193250fad5
Samuel <tuglersamuel@gmail.com>
2020-08-10 21:47:54 +0200

    Removed unnecessary scopes and add email one

--- config/passport.js
+++ config/passport.js
@@ -28,7 +28,7 @@ passport.use(new GoogleStrategy({
                 //if not, create a new user
                 new User({
                     googleId: profile.id,
-                    email: profile.emails[0].value,
+                    email: profile.emails ? profile.emails[0].value : "",
                     displayName: profile.displayName,
                     name: profile.name.givenName,
                     profilePicture: profile.photos ? profile.photos[0].value : "https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg",
@@ -44,7 +44,7 @@ passport.use(new FacebookStrategy({
         clientID: secrets.auth.facebook.app_id,
         clientSecret: secrets.auth.facebook.app_secret,
         callbackURL: secrets.auth.facebook.callback_uri,
-        profileFields: ['id', 'displayName', 'name', 'gender', 'picture.type(large)']
+        profileFields: ['id', 'displayName', 'name', 'picture.type(large)', 'emails']
     },
     function(accessToken, refreshToken, profile, done) {
         User.findOne({facebookId: profile.id}).then((currentUser)=>{
@@ -55,7 +55,7 @@ passport.use(new FacebookStrategy({
                 //if not, create a new user
                 new User({
                     facebookId: profile.id,
-                    email: profile.email,
+                    email: profile.emails ? profile.emails[0].value : "",
                     displayName: profile.displayName,
                     name: profile.name.givenName,
                     profilePicture: profile.photos ? profile.photos[0].value : "https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg",
@@ -70,7 +70,8 @@ passport.use(new FacebookStrategy({
 passport.use(new TwitterStrategy({
         consumerKey: secrets.auth.twitter.consumerKey,
         consumerSecret: secrets.auth.twitter.consumerSecret,
-        callbackURL: secrets.auth.twitter.callback_uri
+        callbackURL: secrets.auth.twitter.callback_uri,
+        includeEmail: true,
     },
     function(token, tokenSecret, profile, done) {
         User.findOne({twitterId: profile.id}).then((currentUser)=>{
@@ -81,7 +82,7 @@ passport.use(new TwitterStrategy({
                 //if not, create a new user
                 new User({
                     twitterId: profile.id,
-                    email: profile.email,
+                    email: profile.emails ? profile.emails[0].value : "",
                     displayName: profile.displayName,
                     name: profile.username,
                     profilePicture: profile._json.profile_image_url_https,
